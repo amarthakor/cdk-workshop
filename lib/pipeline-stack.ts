77a0b8d83efe42ff7e1d06f485dcb2200ea076fe
Amar <115903355+amarthakor@users.noreply.github.com>
2025-11-11 12:27:29 -0800

    Changed quotes to allow string interpolation

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,11 +1,27 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from "aws-cdk-lib";
+import { Bucket, BlockPublicAccess } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class WorkshopPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
+        // Creates our S3 bucket for the remote git repo
+        const gitBucket = new Bucket(this, "GitBucket", {
+            bucketName: `workshop-git-${this.account}-${this.region}`,
+            versioned: true,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: RemovalPolicy.DESTROY, // this is for workshop cleanup -- use RETAIN in production env's
+            autoDeleteObjects: true,
+        })
 
+        // Output bucket name for easy reference
+        new CfnOutput(this, "GitBucketName", {
+            value: gitBucket.bucketName,
+            description: 'S3 bucket name for remote git repo',
+        })
+
+        // Pipeline code goes here
     }
 }
 
